5280b51e77d1472c6ff5cdf1eaeb5a55589d76d5
Bintang Rezeka Ramadani <bintangrezeka@gmail.com>
2022-06-07 22:42:03 +0700

    ✨ feat:add dockerfile to deploy

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+FROM ubuntu:jammy
+WORKDIR /app
+RUN apt-get update -y
+RUN apt-get install -y python3-pip python3 tesseract-ocr tesseract-ocr-ind ffmpeg libsm6 libxext6
+
+COPY . /app
+
+RUN pip install -r requirements.txt
+
+EXPOSE 8080
+CMD [ "python3", "ocr-restapi.py" ]
--- ocr-restapi.py
+++ ocr-restapi.py
@@ -10,11 +10,13 @@ import os
 
 app = Flask(__name__)
 UPLOAD_FOLDER = 'uploaded_img/'
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER 
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
 
 @app.route("/")
 def index():
     return "Halo"
+
 
 def containsNumber(value):
     if True in [char.isdigit() for char in value]:
@@ -33,15 +35,16 @@ def containsNumber(value):
 #             total.append(element)
 #             if containsNumber(element):
 #                 break
-    
+
 #     return total
+
 
 def findAmount(total):
     temp = []
     for element in total:
         if containsNumber(element):
             temp.append(element)
-    
+
     if len(temp) > 1:
         amount = ""
         for element in temp:
@@ -50,7 +53,8 @@ def findAmount(total):
     else:
         return temp
 
-@app.route('/uploader', methods = ['POST'])
+
+@app.route('/uploader', methods=['POST'])
 def upload_file():
     if request.method == 'POST':
         f = request.files['file']
@@ -59,30 +63,35 @@ def upload_file():
         filename = secure_filename(f.filename)
 
         # save file to /static/uploads
-        filepath = os.path.join(app.config['UPLOAD_FOLDER'],filename)
+        filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
         f.save(filepath)
-        
+
         # load the example image and convert it to grayscale
         image = cv2.imread(filepath, 0)
 
         # save the processed image in the /static/uploads directory
-        ofilename = os.path.join(app.config['UPLOAD_FOLDER'],"{}.png".format(os.getpid()))
+        ofilename = os.path.join(
+            app.config['UPLOAD_FOLDER'], "{}.png".format(os.getpid()))
         cv2.imwrite(ofilename, image)
-        
+
         # perform OCR on the processed image
         myconfig = r"--psm 11 --oem 3"
-        data = pytesseract.image_to_data(image, config=myconfig, output_type=Output.DICT)
+        data = pytesseract.image_to_data(
+            image, config=myconfig, output_type=Output.DICT)
         amount_boxes = len(data['text'])
         texts = []
 
         for i in range(amount_boxes):
             if float(data['conf'][i]) > 75:
-                (x, y, width, height) = (data['left'][i], data['top'][i], data['width'][i], data['height'][i])
-                image = cv2.rectangle(image, (x, y), (x+width, y+height), (0,255,0), 2)
+                (x, y, width, height) = (
+                    data['left'][i], data['top'][i], data['width'][i], data['height'][i])
+                image = cv2.rectangle(
+                    image, (x, y), (x+width, y+height), (0, 255, 0), 2)
                 texts.append(data['text'][i])
         if len(texts) == 0:
-            texts.append((pytesseract.image_to_string(Image.open(ofilename))).split())
-        
+            texts.append((pytesseract.image_to_string(
+                Image.open(ofilename))).split())
+
         final_amounts = findAmount(texts)
 
         # remove the processed image
@@ -90,17 +99,18 @@ def upload_file():
 
         if len(final_amounts) != 0:
             data_returned = {
-                "message" : "1",
-                "amounts" : final_amounts,
+                "message": "1",
+                "amounts": final_amounts,
             }
         else:
             data_returned = {
-                "message" : "0",
-                "amounts" : final_amounts,
+                "message": "0",
+                "amounts": final_amounts,
             }
 
         return jsonify(data_returned)
 
+
 if __name__ == '__main__':
     # port = int(os.environ.get('PORT', 5000))
-    app.run(host="127.0.0.1", port=8080, debug=True)
+    app.run(host="0.0.0.0", port=8080, debug=True)
